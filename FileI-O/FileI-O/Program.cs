9d469cbb4d16403de958132ffab628babe855aae
shital bramhe <shitalbramhe@gmail.com>
2021-12-07 09:52:07 +0530

    [shital] ADD: Text file read and edited using streamreader and streamwriter

---
+++ FileI-O/FileI-O/Program.cs
@@ -0,0 +1,55 @@
+﻿using FileI_O;
+using System;
+using System.IO;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //Program.FileExists();
+            //Program.ReadAllLines();
+            //Program.ReadAllText();
+
+            ReadthroughStreamReader readthroughStreamReader = new ReadthroughStreamReader();
+            readthroughStreamReader.WriteUsingStreamWriter();
+            //readthroughStreamReader.ReadFromStreamReader();
+        }
+        public static void FileExists()
+        {
+            String path = @"E:\RFP\FileI-O\FileI-O\FileI-O\Example.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+            }
+            Console.ReadKey();
+        }
+        public static void ReadAllLines()
+        {
+            String path = @"E:\RFP\FileI-O\FileI-O\FileI-O\Example.txt";
+            String[] Lines;
+            Lines = File.ReadAllLines(path);
+
+            Console.WriteLine("lines[0]");
+            Console.WriteLine("lines[1]");
+
+            Console.ReadKey();
+        }
+        public static void ReadAllText()
+        {
+            String path = @"E:\RFP\FileI-O\FileI-O\FileI-O\Example.txt";
+            String lines;
+            lines = File.ReadAllText(path);
+
+            Console.WriteLine("lines");
+           
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
---
+++ FileI-O/FileI-O/ReadthroughStreamReader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileI_O
+{
+    public class ReadthroughStreamReader
+    {
+        public void WriteUsingStreamWriter()
+        {
+            String path = @"E:\RFP\FileI-O\FileI-O\FileI-O\Example.txt";
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine("Hello world - .Net is awesome");
+                sr.Close();
+
+                Console.WriteLine(File.ReadAllText(path));
+            }
+            Console.ReadKey();
+        }
+        public void ReadFromStreamReader()
+        {
+            String path = @"E:\RFP\FileI-O\FileI-O\FileI-O\Example.txt";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                
+                String s = " ";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
